401d226732b57134f994bff89597141e18794183
Artem_Vinokhodov <a.vinohod@gmail.com>
2024-09-08 01:17:39 +0200

    Remove large files from Git tracking

--- cypress/Cypress/resources/app/packages/v8-snapshot-require/dist/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EMBEDDED = void 0;
-/**
- * Name under which the snapshotted file will appear in tools like DevTools.
- */
-exports.EMBEDDED = '<embedded>';
